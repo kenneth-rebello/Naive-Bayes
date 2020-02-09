a09b6be5693e7869535093822d64e4ad68eddbfd
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2020-02-09 22:22:13 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,55 @@
+============== RESTART: C:\Users\kenre\Desktop\College\BI\expt1.py =============
+dataset and dataset2 are the available files for testing
+Enter filename, must be a csv
+dataset
+###THIS ALGORITHM IS SPECIFIC TO DATASETS HAVING ONLY ONE CLASS VARAIBLE SPECIFICALLY IN THE LAST COLUMN###
+Enter tuple to test in following lines
+Enter age
+middle
+Enter income
+medium
+Enter student
+no
+Enter credit
+excellent
+Tuple to be tested: 
+['middle', 'medium', 'no', 'excellent']
+No
+>>>
+
+============== RESTART: C:\Users\kenre\Desktop\College\BI\expt1.py =============
+dataset and dataset2 are the available files for testing
+Enter filename, must be a csv
+dataset
+###THIS ALGORITHM IS SPECIFIC TO DATASETS HAVING ONLY ONE CLASS VARAIBLE SPECIFICALLY IN THE LAST COLUMN###
+Enter tuple to test in following lines
+Enter age
+young
+Enter income
+high
+Enter student
+yes
+Enter credit
+fair
+Tuple to be tested: 
+['young', 'high', 'yes', 'fair']
+Yes
+>>>
+
+============== RESTART: C:\Users\kenre\Desktop\College\BI\expt1.py =============
+dataset and dataset2 are the available files for testing
+Enter filename, must be a csv
+dataset
+###THIS ALGORITHM IS SPECIFIC TO DATASETS HAVING ONLY ONE CLASS VARAIBLE SPECIFICALLY IN THE LAST COLUMN###
+Enter tuple to test in following lines
+Enter age
+senior
+Enter income
+high
+Enter student
+no
+Enter credit
+excellent
+Tuple to be tested: 
+['senior', 'high', 'no', 'excellent']
+Yes
